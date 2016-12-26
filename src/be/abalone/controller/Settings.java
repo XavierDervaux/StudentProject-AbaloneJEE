c96d57994906e456a747dc54859d2e55f8784060
Xavier Dervaux <xavier.dervaux@outlook.be>
2016-12-26 19:24:16 +0100

    Settings terminé 	modified:   src/be/abalone/controller/Settings.java

--- src/be/abalone/controller/Settings.java
+++ src/be/abalone/controller/Settings.java
@@ -5,7 +5,11 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 import be.abalone.model.Identification;
+import be.abalone.model.Joueur;
+import be.abalone.model.Utilitaire;
 
 public class Settings  extends HttpServlet{
 	private static final long serialVersionUID = 1L;
@@ -25,7 +29,18 @@ public class Settings  extends HttpServlet{
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession sessions = request.getSession();
+    	String mail = null, mdp = null;
+    	Joueur actuel = (Joueur) sessions.getAttribute("joueur"); //On est sur que le joueur existe car on vérifie estConnecté avant d'arriver ici
 
+		mail = Utilitaire.getValeurChamp(request, "emailInscription");
+		mdp = Utilitaire.getValeurChamp(request, "passwordInscription");
+		actuel.setEmail(mail);
+		actuel.setMdp(Utilitaire.cryptPassword(mdp));
+		
+		actuel.updateBDD();
+		sessions.setAttribute("joueur", actuel); //On met à jour la session
+		response.sendRedirect("/Abalone/menu.html");
     }
 
 }
