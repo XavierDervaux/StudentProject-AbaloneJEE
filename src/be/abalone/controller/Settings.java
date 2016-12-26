af8a89c96304918961cb2f349e6c926b0ab198fb
Xavier Dervaux <xavier.dervaux@outlook.be>
2016-12-26 19:13:43 +0100

    Orthographe 	new file:   src/be/abalone/controller/Settings.java

---
+++ src/be/abalone/controller/Settings.java
@@ -0,0 +1,31 @@
+package be.abalone.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import be.abalone.model.Identification;
+
+public class Settings  extends HttpServlet{
+	private static final long serialVersionUID = 1L;
+	
+	public Settings() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Boolean estConnecte = Identification.estConnecte(request.getSession(), request.getCookies()); 
+		
+		if(estConnecte){ //On charge la page paramètre
+			this.getServletContext().getRequestDispatcher("/WEB-INF/setting.jsp").forward(request, response);
+    	} else { //N'est pas encore connecté, on affihce le formulaire de connexion/inscription
+    		response.sendRedirect("/Abalone/index.html");
+    	}
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+}
